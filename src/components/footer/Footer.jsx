1f092493a939b18835f3edd62ad50bfd3efe5ee9
unknown <duta.shoshiashvili@gmail.com>
2023-12-30 13:48:13 +0400

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
+
+function Footer() {
+  return (
+    <div className={styles.container}>
+      
+      
+      
+    </div>
+  );
+}
+
+export default Footer;
